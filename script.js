7ef1f29609fec484c047c8622ed454d9c126b374
SamJamCul <samueljamesculley@gmail.com>
2018-01-02 15:17:45 +0000

    changes to a random colour now

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const grid = document.querySelector('.grid');
-let gridsize = 16;
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
 function drawGrid(gridsize = 16) {
   for (i = 0; i < gridsize*gridsize; i++) {
     let newdiv = document.createElement('div');
     newdiv.classList.add('square');
     newdiv.addEventListener("mouseover", function(){
-      newdiv.style.backgroundColor = "red";
+      newdiv.style.backgroundColor = getRandomColor();
     })
     grid.appendChild(newdiv);
   }
 }
-
-
 
 drawGrid()
